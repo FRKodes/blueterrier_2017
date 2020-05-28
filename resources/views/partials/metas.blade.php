d2229b926e25e6ec1907475631d7dbaf059309b9
Antonio Calderón Jerónimo <tony@MacBook-Air-de-Usuario.local>
2020-05-28 17:55:23 -0500

    Meta tags fixes

--- resources/views/partials/metas.blade.php
+++ resources/views/partials/metas.blade.php
@@ -3,7 +3,9 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Cache-control" content="public">
 <meta name="last-modified" content="2017-06-12">
-<meta name="description" content="@yield('description', 'Somos un estudio creativo, especializado en productos digitales como estrategias digitales, diseño y desarrollo web, branding y marketing digital con sede en Guadalajara Jalisco y presencia en la zona del caribe en Playa del Carmen, Quintana Roo, México. Diseñamos logos, UX, Desarrollo Web.')">
+<meta name="description" content="@yield('description', 'Diseñamos marcas memorables, Desarrollamos Estrategias Digitales que impactan positivamente a tu audiencia, Creamos tu Página Web y Tienda Online, MKT digital orientado a cumplir objetivos.')">
 <meta name="keywords" content="Logo design, Web development, web development guadalajara, web development mexico, web design, web design mexico, web design guadalajara, outsourcing mexico, digital strategy, logo design mexico, logo design guadalajara, near shore design, near shore development, design and advertising, advertising mexico, responsive design, UX, user experience, startup, user experience design">
-<title>Blue Terrier Studio - @yield('title', 'Estudio de Diseño, Desarrollo Web y MKT Digital en Guadalajara Jalisco México. #doggieStyle')</title>
+<title>Blue Terrier Studio - @yield('title', 'Diseño, Branding, Desarrollo Web y MKT Digital en Guadalajara México')</title>
 <link rel="icon" href="favicon.png" type="image/png" sizes="32x32">
+
+
